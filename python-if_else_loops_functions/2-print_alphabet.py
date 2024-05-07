5e8db8960f0a20db8f2ae0050a2565d25b843266
Epicder <epicder11@gmail.com>
2024-05-07 18:12:51 +0000

    commit

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for c in (chr(i) for i in range(97, 123)):
-    if c != chr(101) and c != chr(113):
-        print (f"{c}", end="")
+
+print("{}".format("".join(chr(i) for i in range(97, 123))), end="")
+
