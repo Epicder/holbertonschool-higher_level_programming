48f12d8ce01c4eb6b157944e48eec31cd38123d1
Epicder <epicder11@gmail.com>
2024-05-09 16:12:50 +0000

    commit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if number == 0:
-        print("{}".format(number), end="")
-    elif number < 0:
-        numcpy = abs(number)
-        lastnum = numcpy % 10
-        lastnum += 5
-        lastnum += 1
-        print("{}".format(lastnum), end="")
-    elif number > 0:
-        lastnum = number % 10
-        print("{}".format(lastnum), end="")
-    return number % 10
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
