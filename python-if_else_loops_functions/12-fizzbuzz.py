4bcf54a7f2caa6f5cd75b7c04493d4221185770b
Epicder <epicder11@gmail.com>
2024-05-09 17:06:33 +0000

    commit

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+
+def fizzbuzz():
+    for numbers in range(1, 101):
+        numcpy = numbers % 3
+        numcpyy = numbers % 5
+        if(numcpy == 0):
+            print("Fizz", end=" ")
+        elif(numcpyy == 0):
+            print("Buzz", end=" ")
+        else:
+            print(f"{numbers}", end=" ")
+    
