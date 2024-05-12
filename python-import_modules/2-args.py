0bf4ab21559a48e2be15a328774ee6f376dd6579
Epicder <epicder11@gmail.com>
2024-05-12 23:24:42 +0000

    commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if (len(sys.argv) - 1) == 0:
         print("0 arguments.")
     elif (len(sys.argv) - 1) == 1:
-        print ("1 argument:")
+        print("1 argument:")
         print("1: {}".format(str(sys.argv[1])))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
