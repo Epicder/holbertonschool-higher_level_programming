48ad45d3239eff433c6c1488b73a1e74d0cb5361
Epicder <epicder11@gmail.com>
2024-05-14 14:11:34 +0000

    commit

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx > len(my_list) or idx < 0:
+    if idx > len(my_list):
+        return None
+    elif idx < 0:
         return None
     else:
         return my_list[idx]
