fe63e543359fcfd048f9c799a36fec2a8fb26bc1
Epicder <epicder11@gmail.com>
2024-05-14 17:33:07 +0000

    commit

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    newstr = ''
+    for i in range(len(my_string)):
+        if my_string[i] not in ['c', 'C']:
+            newstr += my_string[i]
+    return newstr
