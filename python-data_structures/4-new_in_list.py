c82c652559182c990eb892f77734e00ad4d546b2
Epicder <epicder11@gmail.com>
2024-05-14 17:10:48 +0000

    commit

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         my_listcp = my_list.copy()
-        return my_lsitcp
+        return my_listcp
     else:
         my_listcp = my_list.copy()
         my_listcp[idx] = element
