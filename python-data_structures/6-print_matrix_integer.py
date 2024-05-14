c2292afeb19353bf97e28d2efb2084505ddc98a2
Epicder <epicder11@gmail.com>
2024-05-14 18:13:50 +0000

    commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,5 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for elem in row:
-            print("{:d}".format(elem), end=" ")
-        print()
+        row_str = " ".join("{:d}".format(elem) for elem in row)
+        print(row_str)
