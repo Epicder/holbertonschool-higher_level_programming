cefe3ab6e12b5d733885cef548362290f38db0c3
Epicder <epicder11@gmail.com>
2024-05-16 12:36:46 +0000

    commit

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-      a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
-      b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
-      
-      tuple_result = (a[0] + b[0], a[1] + b[1])
-      return tuple_result
-      
+    a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
+    b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
+    tuple_result = (a[0] + b[0], a[1] + b[1])
+    return tuple_result
