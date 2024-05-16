b22e871cd0908072ac5b208c6787a3d3810040eb
Epicder <epicder11@gmail.com>
2024-05-16 12:58:24 +0000

    commit

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    for i in range (len(my_list)):
-        if my_list[i] > my_list[i+1]:
+    if not my_list:
+        return None
+    biggest = my_list[0]
+    for i in range (1, len(my_list)):
+        if my_list[i] > biggest:
             biggest = my_list[i]
             return biggest
