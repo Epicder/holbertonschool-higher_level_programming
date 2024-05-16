1baf083d92b74e203422b6539d22af1774c4df8e
Epicder <epicder11@gmail.com>
2024-05-16 13:29:20 +0000

    commit

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,6 +2,6 @@
 
 def divisible_by_2(my_list=[]):
     new_list = []
-    for i in range (len(my_list)):
+    for i in range(len(my_list)):
         new_list.append(my_list[i] % 2 == 0)
     return new_list
