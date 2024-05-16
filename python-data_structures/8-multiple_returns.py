c9016b8139f984c614b1894ed6aeb321731a1eba
Epicder <epicder11@gmail.com>
2024-05-16 12:46:37 +0000

    commit

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,8 @@
 def multiple_returns(sentence):
     if sentence == "":
         sentence = None
+        tuple = len(sentence), sentence[0]
+        return tuple
     else:
         tuple = len(sentence), sentence[0]
         return tuple
