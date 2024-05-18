e1861ce73e0cac42eb9b66d9ecfd2ecf84b909fc
Epicder <epicder11@gmail.com>
2024-05-18 17:59:22 +0000

    commit

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for i in range (len(new_list)):
+    for i in range(len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
 
