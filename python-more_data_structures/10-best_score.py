4e4b114c960b8f75af508b54b6def6fc07a2497d
Epicder <epicder11@gmail.com>
2024-05-18 23:04:28 +0000

    commit

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_key = None
+    best_value = None
+    for key, value in a_dictionary.items():
+        if best_value is None or value > best_value:
+            best_value = value
+            best_key = key
+    return best_key
+            
