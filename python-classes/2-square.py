33cd8485b18fc4a00e939d1b6027e5ee44c34d7f
Epicder <epicder11@gmail.com>
2024-05-20 00:01:13 +0000

    commit

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' Square class:
+'''
+class Square:
+    '''square
+    '''
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
