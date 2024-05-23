3fcb429075e826617c4bfca4eb4a5ca284c22d6d
Epicder <epicder11@gmail.com>
2024-05-23 21:40:55 +0000

    commit

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -13,11 +13,11 @@ class Rectangle:
 
     @property
     def width(self):
-        return self.width
+        return self.__width
 
     @property
     def height(self):
-        return self.height
+        return self.__height
 
     @width.setter
     def width(self, value):
