abb831bc19b6131de1c51c2a5c1d2cd2cf5291ba
Epicder <epicder11@gmail.com>
2024-05-23 21:37:13 +0000

    commit

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 ''' Class: New class
 '''
 
@@ -24,7 +25,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        return self._width = value
+        self._width = value
 
     @height.setter
     def height(self, value):
@@ -32,4 +33,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        return self._height = value
+        self._height = value
