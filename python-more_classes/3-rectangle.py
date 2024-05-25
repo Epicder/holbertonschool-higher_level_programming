12aea55b8b43c1bd56bb8c783181ae71515de95b
Epicder <epicder11@gmail.com>
2024-05-25 01:12:03 +0000

    commit

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
-            return None
+            return ""
         rectang = []
         for i in range(self.__height):
             rectang.append("#" * self.__width)
