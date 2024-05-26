88395e0328876b9a7a114502ee2bdb92b7a6a480
Epicder <epicder11@gmail.com>
2024-05-26 20:30:34 +0000

    commit

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -45,8 +45,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            return 2 * (self.__height + self.__width)        
-        
+            return 2 * (self.__height + self.__width)
+
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
