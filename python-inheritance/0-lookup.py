018078caaa4f8dbf8c806a8b5c8f0a9570ed37d6
Epicder <epicder11@gmail.com>
2024-05-27 01:28:50 +0000

    commit

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-
+""" This module provides a function to list the attributes and methods of an object."""
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+    """
     return dir(obj)
