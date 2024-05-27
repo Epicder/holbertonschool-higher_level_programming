db843d9861b25df0e2182b73c92313e31c45ed20
Epicder <epicder11@gmail.com>
2024-05-27 02:37:40 +0000

    commit

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+"""create a class"""
+
+
+class BaseGeometry:
+    """class"""
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
