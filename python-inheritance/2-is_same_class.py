2b1a825e55c2a4f896e7ba6dc8252874dd84dd73
Epicder <epicder11@gmail.com>
2024-05-27 02:14:25 +0000

    commit

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """returns True if the object is exactly an instance of the specified class"""
-def is_same_class(obj, a_class):
 
-    
+
+def is_same_class(obj, a_class):
     """logic"""
     if type(obj) is a_class:
         return True
