d5c482055a40c09bd2aa3869c7dba51b35e488f1
Epicder <epicder11@gmail.com>
2024-05-28 13:57:41 +0000

    commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+""" function that read a file"""
+
 
 def read_file(filename=""):
+    """code"""
     with open(filename, mode="r", encoding="utf-8") as mfile:
         text = mfile.read()
         print(text)
