89902d347c95422fb00c0dc29ce6135556b74e95
Epicder <epicder11@gmail.com>
2024-05-30 04:54:12 +0000

    commit

---
+++ python-input_output/9-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""class"""
+
+class Student:
+    first_name = ""
+    last_name = ""
+    age = ""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
