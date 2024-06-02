e79028e0bf148f06cdbe4c7970b217b9d036ad75
Epicder <epicder11@gmail.com>
2024-06-02 00:18:02 +0000

    commit

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,11 +4,11 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
-    with open(filename, w) as jfile:
+    with open(filename, 'w') as jfile:
         json.dump(data, jfile)
 
 
 def load_and_deserialize(filename):
-    with open(filename, r) as jfile:
-        code = json.load(data, jfile)
+    with open(filename, 'r') as jfile:
+        code = json.load(jfile)
     return code
