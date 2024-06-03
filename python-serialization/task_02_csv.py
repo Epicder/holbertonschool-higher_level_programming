bb4346d69ffeea79f17b1cd0b027ee2ded4d0e79
Epicder <epicder11@gmail.com>
2024-06-03 00:36:39 +0000

    commit

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -7,11 +7,11 @@ import json
 def convert_csv_to_json(csv_file):
     try:
         with open(csv_file, mode='r', encoding='utf-8') as csv_f:
-            csv_r = csv.dictReader(csv_f)
+            csv_r = csv.DictReader(csv_f)
             code = [row for row in csv_r]
 
             with open('data.json', mode='w', encoding='utf-8') as json_file:
-                json_file.dump(code, json_file)
+                json.dump(code, json_file, indent=4)
 
             return True
     except FileNotFoundError:
