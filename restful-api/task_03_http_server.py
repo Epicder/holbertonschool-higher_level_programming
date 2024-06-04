30e10cd850ec432890b5e641ea5f2c15808cd479
Epicder <epicder11@gmail.com>
2024-06-04 17:49:33 +0000

    commit

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import http.server as srv
+import socketserver
+PORT = 8000
+class Server(srv.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+
+if __name__ == "__main__":
+    with socketserver.TCPServer(("", PORT), Server) as httpd:
+        print("Serving at port", PORT)
+        httpd.serve_forever()
