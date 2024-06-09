0a14062f842446569f7d23bbe40833380f31103f
Epicder <epicder11@gmail.com>
2024-06-09 21:16:35 +0000

    commit

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,43 @@
+#!/bin/usr/python3
+
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        return 2 * (self.height + self.width)
+
+
+def shape_info(shape):
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
