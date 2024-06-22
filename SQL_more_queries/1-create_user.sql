cfdc3913bb4e6bca785c0899c08cfe923cd0002c
Epicder <epicder11@gmail.com>
2024-06-22 16:29:53 -0300

    COMMIT

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Create a user
 
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' IF NOT EXISTS;
-GRANT * ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT * ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH *;
