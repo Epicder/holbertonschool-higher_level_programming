5bc67537900bf6ab443eabb6b6299952c43dac64
Epicder <epicder11@gmail.com>
2024-06-22 16:43:54 -0300

    TASK 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create a user
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT * ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH *;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
