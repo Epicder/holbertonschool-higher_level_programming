fd3652ace0567c69c568aa4e464700947da05a66
Epicder <epicder11@gmail.com>
2024-06-22 18:13:55 -0300

    commit

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- create a database and an user
 
-CREATE DATABASE hbtn_0d_2 IF NOT EXISTS;
-CREATE USER IN hbtn_0d_2 IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
