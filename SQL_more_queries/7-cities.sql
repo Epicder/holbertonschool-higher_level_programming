29c48459e796ca7468890cb31da2b6aab19e0a1a
Epicder <epicder11@gmail.com>
2024-06-23 19:42:20 -0300

    commit

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- create database and table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL FOREIGN KEY (hbtn_0d_usa.states.id),
+    name VARCHAR(256) NOT NULL
+);
