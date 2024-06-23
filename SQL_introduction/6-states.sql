a3145b528e72cf86b24823f50e34bd937d0fcb65
Epicder <epicder11@gmail.com>
2024-06-23 02:04:55 -0300

    task 6

---
+++ SQL_introduction/6-states.sql
@@ -0,0 +1,7 @@
+-- create database an table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
