79f2bc7a870c82e1e8c499095ee08df6bdedb358
Epicder <epicder11@gmail.com>
2024-06-23 21:48:29 -0300

    try

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 
-SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows;
-INNER JOIN tv_show_genres ON hbtn_0d_tvshows.id = tv_show_genres.show_id
-ORDER BY hbtn_0d_tvshows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tvshows;
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
