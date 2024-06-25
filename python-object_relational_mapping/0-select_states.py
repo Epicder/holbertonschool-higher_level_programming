f47e32673242129f2b402673cef4e31addb3f41b
Epicder <epicder11@gmail.com>
2024-06-25 11:31:41 -0300

    commit

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 Write a script that lists all states from the database.
@@ -6,8 +5,9 @@ Write a script that lists all states from the database.
 import sys
 import MySQLdb
 
-def get_states():
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+def get_states(username, password, database):
+    """get all states"""
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
@@ -17,4 +17,8 @@ def get_states():
     conn.close()
 
 if __name__ == '__main__':
-    get_states()
+    """comment"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    get_states(username, password, database)
