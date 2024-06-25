d17d13d235ef403cfca3caf7de619a2f68b7af48
Epicder <epicder11@gmail.com>
2024-06-25 13:40:30 -0300

    try?

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+avoid sql injection
+"""
+import sys
+import MySQLdb
+
+
+def get_states(username, password, database, state_name):
+    """get states"""
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    sqlcmd = ("SELECT * FROM states " +
+                "WHERE BINARY name LIKE %s ORDER BY id ASC")
+    cur.execute(sqlcmd, (state_name,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    """comment"""
+    if sys.argv != 5:
+        raise IndexError("out of range")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        state_name = sys.argv[4]
+        get_states(username, password, database, state_name)
