880de18bdbe4fa4d8fc40f2f97688f2af05e46e4
Epicder <epicder11@gmail.com>
2024-06-25 14:17:58 -0300

    task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Write a script that list all states that start with N
+"""
+import sys
+import MySQLdb
+
+
+def get_states(username, password, database):
+    """get states starting with N"""
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    """comment"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    get_states(username, password, database)
