96e503c4ae63f0dfe755fd8dfa37a0525355ebfc
Epicder <epicder11@gmail.com>
2024-07-20 20:01:36 -0300

    task 03

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+import json
+import csv
+app = Flask(__name__)
+
+def read_csv_file(file_path):
+    products = []
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            row['id'] = int(row['id'])
+            row['price'] = float(row['price'])
+            products.append(row)
+    return products
+
+def read_json_file(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+    
+
+@app.route('/products')
+def display_products():
+    source = request.args.get('source')
+    products = []
+
+    if source == 'json':
+        products = read_json_file('products.json')
+    elif source == 'csv':
+        products = read_csv_file('products.csv')
+    else:
+        raise 'Wrong source. Please specify "json" or "csv".'
+
+    return render_template('product_display.html', products=products)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    try:
+        with open('items.json', 'r') as file:
+            data = json.load(file)
+            items_list = data.get("items", [])
+    except FileNotFoundError:
+        items_list = []
+    return render_template('items.html', items=items_list)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
